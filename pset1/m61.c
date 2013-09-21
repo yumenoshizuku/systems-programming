935d934845455f9aa7379f7aacc1f9c1dabca7d1
Fanxing Meng <meng@g.harvard.edu>
2013-09-21 00:01:16 -0400

    test001-017 works

--- pset1/m61.c
+++ pset1/m61.c
@@ -8,9 +8,11 @@
 int allocated = 0;
 int sizeallocated = 0;
 int freed = 0;
-int failed =0;
+int sizefreed = 0;
+int failed = 0;
 int failedsize = 0;
-
+int sztbl[100][4] = {0, 1};
+size_t top = 0xb000000;
 
 void *m61_malloc(size_t sz, const char *file, int line) {
     (void) file, (void) line;   // avoid uninitialized variable warnings
@@ -23,30 +25,101 @@ void *m61_malloc(size_t sz, const char *file, int line) {
     else {
         allocated++;
         sizeallocated += sz;
+        int i;
+        for(i=1; i<=sztbl[0][1]; i++){
+            if(sztbl[i][0] == retptr && sztbl[i][1] == 0){
+                sztbl[i][1] = sz;
+                while (sz%4 != 0){
+                    sz++;
+                }
+                sztbl[i][2] = sz;
+                sztbl[i][3] = (int)(int *)((char *)retptr+sz);
+                
+                return retptr;
+            }
+        }
+        sztbl[(sztbl[0][1]+1)][0] = retptr;
+        sztbl[(sztbl[0][1]+1)][1] = sz;
+        while (sz%4 != 0){
+                    sz++;
+                }
+                sztbl[(sztbl[0][1]+1)][2] = sz;
+                sztbl[(sztbl[0][1]+1)][3] = (int)(int *)((char *)retptr+sz);                
+        sztbl[0][1] += 1;
+//        top = top + (*((uint *)top)) - (*((uint *)top))%16;
         return retptr;
      }
 }
 
 void m61_free(void *ptr, const char *file, int line) {
     (void) file, (void) line;   // avoid uninitialized variable warnings
-    freed++;
+    int i;
+    if (ptr < 0x804b004 || ptr > top){
+        printf("MEMORY BUG: %s:%d: invalid free of pointer %p, not in heap\n", file, line, ptr);
+        return;
+    }
+    for (i=1;i<=sztbl[0][1];i++){
+        if (sztbl[i][0] == ptr && sztbl[i][1] != 0){
+              if((int)(int *)((char *)ptr+sztbl[i][2])!=sztbl[i][3]){
+                printf("MEMORY BUG: %s:%d: detected wild write during free of pointer %p\n", file, line, ptr);
+                return;
+              }
+/*            if(((sztbl[i][1])%4 ==0 && *((char *)ptr+(sztbl[i][1])) != sztbl[i][2])||((sztbl[i][1])%4 !=0 && *((char *)ptr+(sztbl[i][1])+4-(sztbl[i][1])%4) != sztbl[i][2])){
+                printf("MEMORY BUG: %s:%d: detected wild write during free of pointer %p\n", file, line, ptr);
+                return;
+            }
+*/            
+            sizefreed += sztbl[i][1];
+            sztbl[i][1]=0;
+            sztbl[i][2]=0;
+            freed++;
+            break;
+        }
+        if (sztbl[i][0] == ptr && sztbl[i][1] == 0){
+            printf("MEMORY BUG: %s:%d: invalid free of pointer %p\n", file, line, ptr);
+            return;
+        }
+        if (i == sztbl[0][1]){
+            printf("MEMORY BUG: %s:%d: invalid free of pointer %p, not allocated\n", file, line, ptr);
+            return;
+       }
+
+    }
     free(ptr);
 }
 
 void *m61_realloc(void *ptr, size_t sz, const char *file, int line) {
     void *new_ptr = NULL;
-    if (sz)
+    if (sz){
         new_ptr = m61_malloc(sz, file, line);
-    // Oops! In order to copy the data from `ptr` into `new_ptr`, we need
-    // to know how much data there was in `ptr`. That requires work.
-    // Your code here (to fix test008).
-    m61_free(ptr, file, line);
+        if(ptr){
+            uint i;
+            for (i=0; i < ((*((int *)ptr-1))/16)*16; i++){
+                *((char *)new_ptr+i) = *((char *)ptr+i);
+            }
+/*            for (i=1;i<=sztbl[0][1];i++){
+                if(sztbl[i][0] == ptr){
+                    sztbl[(sztbl[0][1])][2] = sztbl[i][2];
+                }
+            }
+*/            
+            m61_free(ptr, file, line);
+        }
+    }
+    else{
+        m61_free(ptr, file, line);
+    }
     return new_ptr;
 }
 
 void *m61_calloc(size_t nmemb, size_t sz, const char *file, int line) {
-    // Your code here (to fix test010).
-    void *ptr = m61_malloc(nmemb * sz, file, line);
+    void *ptr = NULL;
+    if ((long long)nmemb * (long long)sz < (long long)((size_t) -1)) {
+        ptr = m61_malloc(nmemb * sz, file, line);
+    }
+    else {
+        failed++;
+    }
     if (ptr)
         memset(ptr, 0, nmemb * sz);
     return ptr;
@@ -57,7 +130,7 @@ void m61_getstatistics(struct m61_statistics *stats) {
     memset(stats, 255, sizeof(struct m61_statistics));
     
     stats->nactive = allocated - freed;
-    stats->active_size = 0;
+    stats->active_size = sizeallocated - sizefreed;
     stats->ntotal = allocated;
     stats->total_size = sizeallocated;
     stats->nfail = failed;
