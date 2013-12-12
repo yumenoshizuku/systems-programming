9e8b47f484451f66ace7fbd061e69f26137a32db
Fanxing Meng <meng@g.harvard.edu>
2013-12-11 23:33:23 -0500

    debugging trick 2

--- pset6/pong61.c
+++ pset6/pong61.c
@@ -309,6 +309,7 @@ void* pong_thread(void* threadarg) {
         fprintf(stderr, "%.3f sec: warning: %d,%d: "
                 "server returned status %d (expected 200)\n",
                 elapsed(), pa.x, pa.y, conn->status_code);
+    if (conn->content_length >= 10 && conn->content_length <= 11) {
     pthread_mutex_lock(&time_lock);
     if(stop_time == 0 && sscanf(http_truncate_response(conn), "%d OK", &stop_time) && stop_time != 0) {
     	sscanf(http_truncate_response(conn), "+%d STOP", &stop_time);
@@ -326,9 +327,29 @@ void* pong_thread(void* threadarg) {
     } else {
     	pthread_mutex_unlock(&time_lock);
     }
+    	http_receive_response_body(conn);
+    	pthread_cond_signal(&condvar);
+    } else {
 	pthread_cond_signal(&condvar);
     http_receive_response_body(conn);
-    
+    pthread_mutex_lock(&time_lock);
+    if(stop_time == 0 && sscanf(http_truncate_response(conn), "%d OK", &stop_time) && stop_time != 0) {
+    	sscanf(http_truncate_response(conn), "+%d STOP", &stop_time);
+    	pthread_mutex_unlock(&time_lock);
+    	if(stop_time < 1000)
+			usleep(stop_time * 1000);
+		else {
+			sleep(stop_time / 1000);
+			usleep((stop_time % 1000) * 1000);
+		}
+    	pthread_mutex_lock(&time_lock);
+		stop_time = 0;
+		pthread_cond_broadcast(&stop_time_cond);
+    	pthread_mutex_unlock(&time_lock);
+    } else {
+    	pthread_mutex_unlock(&time_lock);
+    }
+    }
     double result = strtod(conn->buf, NULL);
     if (result < 0) {
         fprintf(stderr, "%.3f sec: server returned error: %s\n",
